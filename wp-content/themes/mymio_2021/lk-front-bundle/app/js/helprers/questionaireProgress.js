be69dcfbf2af3c33a081931ee8a913f42e688560
a.sivak <riziy_bes@mail.ru>
2022-03-22 10:50:46 +0300

    fix progress bar

--- wp-content/themes/mymio_2021/lk-front-bundle/app/js/helprers/questionaireProgress.js
+++ wp-content/themes/mymio_2021/lk-front-bundle/app/js/helprers/questionaireProgress.js
@@ -1,6 +1,6 @@
 export const questionaireProgress = () => {
     const container = $('.lk-progress-inner');
-    const all = $('.lk-form__tabs').children().length;
+    const all = $('.lk-form[data-form="2"] .lk-form__tabs').children().length;
     const locked = $('.lk-form__tab.locked').length;
 
     const string = `${locked}/${all}`;
