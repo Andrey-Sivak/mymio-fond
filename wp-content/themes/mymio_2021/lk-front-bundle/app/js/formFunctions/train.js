102f109053ee62e05c3ffd8fcc19394c299c8d80
a.sivak <riziy_bes@mail.ru>
2022-03-26 17:31:57 +0300

    implemented class for separate ranges

--- wp-content/themes/mymio_2021/lk-front-bundle/app/js/formFunctions/train.js
+++ wp-content/themes/mymio_2021/lk-front-bundle/app/js/formFunctions/train.js
@@ -2,80 +2,92 @@
     const trains = $('.train');
 
     trains.each(function () {
-        const input = $(this).find('.train__hide-input');
-        const trainItems = $(this).find('.train__item_inner');
-        let start = null;
-        let end = null;
-
-        train.addEventListener('click', selectRange);
-
-        function selectRange(e) {
-            const target = e.target;
-
-            if (!target.classList.contains('train__item_inner')) return;
-
-            const value = parseInt(target.innerHTML);
-
-            if (!start) {
-                start = value;
-                target.parentElement.classList.add('active');
-                setInput(start, end);
-                return;
-            }
-
-            if (start === value || end === value) {
-                activeRange(0, 0);
-                return;
-            }
-
-            if (start > value) {
-                end = start;
-                start = value;
-                activeRange(start, end);
-            } else if (value > start && value < end) {
-                start = value;
-                activeRange(start, end);
-            } else {
-                end = value;
-                activeRange(start, end);
-            }
-        }
-
-        function activeRange(start, end) {
-            setInput(start, end);
-            const activeItems = [...document.querySelectorAll('.train__item.active')];
-
-            if (activeItems.length) {
-                activeItems.forEach(a => a.classList.remove('active'));
-            }
-
-            if (start === end) {
-                start = null;
-                end = null;
-                return;
-            }
-
-            trainItems.forEach(t => {
-                const number = parseInt(t.innerHTML);
-                if (number >= start && number <= end) t.parentElement.classList.add('active');
-            })
-        }
-
-        function setInput(start = 0, end = 0) {
-            const startStr = start ? start + '' : 0;
-            const endStr = end ? end + '' : 0;
-
-            if (!startStr) {
-                input.val('');
-                return;
-            }
-
-            if (!endStr) {
-                input.val(startStr);
-                return;
-            }
-
-            input.val(`${startStr}-${endStr}`);
-        }
+        const train = new Train($(this));
+        train.init();
     });
 })();
+
+function Train(element) {
+    this.contextElement = $(element);
+    this.input = this.contextElement.find('.train__hide-input');
+    this.trainItems = this.contextElement.find('.train__item_inner');
+    this.activeItems = this.contextElement.find('.train__item.active');
+    this.start = null;
+    this.end = null;
+
+    this.selectRange = (e) => {
+        const target = e.target;
+
+        if (!target.classList.contains('train__item_inner')) return;
+
+        const value = parseInt(target.innerHTML);
+
+        if (!this.start) {
+            this.start = value;
+            target.parentElement.classList.add('active');
+            this.setInput(this.start, this.end);
+            return;
+        }
+
+        if (this.start === value || this.end === value) {
+            this.activeRange(0, 0);
+            return;
+        }
+
+        if (this.start > value) {
+            this.end = this.start;
+            this.start = value;
+            this.activeRange(this.start, this.end);
+        } else if (value > this.start && value < this.end) {
+            this.start = value;
+            this.activeRange(this.start, this.end);
+        } else {
+            this.end = value;
+            this.activeRange(this.start, this.end);
+        }
+    }
+
+    this.activeRange = (start, end) => {
+        this.setInput(start, end);
+
+        this.activeItems = this.contextElement.find('.train__item.active');
+
+        if (this.activeItems.length) {
+            this.activeItems.each(function () {
+                $(this).removeClass('active');
+            });
+        }
+
+        if (start === end) {
+            this.start = null;
+            this.end = null;
+            return;
+        }
+
+        this.trainItems.each(function () {
+            const number = parseInt($(this).html());
+            if (number >= start && number <= end) $(this).parent().addClass('active');
+        })
+    }
+
+    this.setInput = (start = 0, end = 0) => {
+        const startStr = start ? start + '' : 0;
+        const endStr = end ? end + '' : 0;
+
+        if (!startStr) {
+            this.input.val('');
+            return;
+        }
+
+        if (!endStr) {
+            this.input.val(startStr);
+            return;
+        }
+
+        this.input.val(`${startStr}-${endStr}`);
+    }
+
+    this.init = () => {
+        this.contextElement.on('click', this.selectRange);
+    }
+}
