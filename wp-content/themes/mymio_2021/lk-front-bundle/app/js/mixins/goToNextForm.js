6bd6c01e1a19ef593223152f2175acac00093d18
a.sivak <riziy_bes@mail.ru>
2022-04-07 12:03:45 +0200

    add defining ill stage js

--- wp-content/themes/mymio_2021/lk-front-bundle/app/js/mixins/goToNextForm.js
+++ wp-content/themes/mymio_2021/lk-front-bundle/app/js/mixins/goToNextForm.js
@@ -1,8 +1,10 @@
 import {questionaireProgress} from "../helprers/questionaireProgress";
+import {getStage} from "./getStage";
 
 export const goToNextForm = (dataFormCurrent) => {
 
     const currentTopTab = $('.lk-form.active').data('form');
+    const isNeurology = parseInt(currentTopTab) === 2;
     if (parseInt(currentTopTab) !== 2) return;
 
     const tabClass = 'lk-form__tab';
@@ -13,6 +15,32 @@ export const goToNextForm = (dataFormCurrent) => {
     const currentForm = $(`.${formClass}[data-form="${dataFormCurrent}"]`);
     currentTab.addClass('locked');
     currentForm.addClass('locked');
+
+    if (isNeurology) {
+        const stageDependForms = $('form[data-stage]');
+
+        const moveAbilities = $('select[data-elma="mank_motor_abilities_2"]').val();
+        const lostAge = $('select[data-elma="mank_neuro_lost_ability"]').val();
+        const stage = getStage(2, moveAbilities, lostAge);
+
+        if (stage) {
+            stageDependForms.each(function () {
+                const stages = $(this).data('stage');
+                const currentStage = stage.toString();
+
+                if (!stages.includes(currentStage)) {
+
+                    const tabCount = $(this)
+                        .parent().parent().parent()
+                        .data('form');
+
+                    const tab = $(`.${tabClass}[data-tab="${tabCount}"]`);
+
+                    tab.addClass('locked');
+                }
+            })
+        }
+    }
 
     let tabsString = '';
 
