0bc46e61d91d4dc6600a51a28490cd5c319ffb25
a.sivak <riziy_bes@mail.ru>
2022-04-11 01:37:47 +0200

    remove unnecessary files

--- wp-content/themes/mymio_2021/lk-front-bundle/app/js/main.js
+++ wp-content/themes/mymio_2021/lk-front-bundle/app/js/main.js
@@ -4,27 +4,14 @@
 // import './getUserDataFromElma';
 
 // view
-// import './formFunctions/select';
-// import './formFunctions/checkboxes';
-// import './formFunctions/datepicker';
-// import './formFunctions/train';
-// import {maskInit} from "./formFunctions/inputMask";
 // import {selectMkb} from "./formFunctions/selectMkb";
 // import {checkLastActiveTabs, checkLockedTabs, tabs} from "./utils/tabs";
 // import './forms';
 // import './mixins/conditionFields';
-// import './formFunctions/yaMaps';
 
 // './helprers/questionaireProgress';
 
-// checkLastActiveTabs('.lk-tab');
-// checkLastActiveTabs('.lk-form__tab');
 // checkLockedTabs();
-
-// tabs('.lk-tab', '.lk-form');
-// tabs('.lk-form__tab', '.lk-form__tab_content');
-
-// maskInit();
 
 // selectMkb('mank_accompanying_diagnosis_2');
 
@@ -32,5 +19,3 @@
 import {AccountClass} from "./classes/AccountClass";
 const account = new AccountClass();
 account.init();
-
-//TODO: fix if error
