ca6c9b988f362e9d19448eb5e000537782ba9d85
a.sivak <riziy_bes@mail.ru>
2022-04-11 02:24:47 +0200

    add definition type for form field

--- wp-content/themes/mymio_2021/lk-front-bundle/app/js/classes/FormFieldClass.js
+++ wp-content/themes/mymio_2021/lk-front-bundle/app/js/classes/FormFieldClass.js
@@ -5,31 +5,38 @@ import {SelectClass} from "./FormFields/SelectClass";
 import {CheckboxClass} from "./FormFields/CheckboxClass";
 
 export const FormFieldClass = function (formField) {
-    this.instance = null;
+    this.formFieldClass = null;
+    this.type = () => {
+        if (formField.hasClass('check')) {
+            return 'checkbox';
+        } else if (formField.find('.select').length) {
+            return 'select';
+        } else if (formField.hasClass('radio')) {
+            return 'radio';
+        } else {
+            return 'input';
+        }
+    }
 
     this.setType = () => {
-        if (formField.hasClass('check')) {
-            this.instance = new CheckboxClass(formField);
-            return;
+        switch (this.type()) {
+            case 'checkbox':
+                this.formFieldClass = new CheckboxClass(formField);
+                break;
+            case 'select':
+                this.formFieldClass = new SelectClass(formField);
+                break;
+            case 'input':
+                this.formFieldClass = new InputClass(formField);
+                break;
         }
-
-        if (formField.find('.select').length) {
-            this.instance = new SelectClass(formField);
-            return;
-        }
-
-        if (formField.hasClass('radio')) {
-            return;
-        }
-
-        this.instance = new InputClass(formField);
     }
 
     this.init = () => {
         this.setType();
 
-        if (this.instance) {
-            this.instance.init();
+        if (this.formFieldClass) {
+            this.formFieldClass.init();
         }
     }
 }
