e7a762bd043c5cd67005c9ab1bebad76f0d0f1b4
a.sivak <riziy_bes@mail.ru>
2022-04-20 17:35:43 +0200

    add settings to phone confirmation field

--- wp-content/themes/mymio_2021/template-parts/form-phone-confirm-template.php
+++ wp-content/themes/mymio_2021/template-parts/form-phone-confirm-template.php
@@ -9,25 +9,25 @@ $question = $object['question'];
 <div class="contact-form__form-field phone-confirm">
 
     <?php if ($question) : ?>
-        <p class="contact-form__form-field_note"><?= $question; ?></p>
+        <p class="contact-form__form-field_note"><?php echo $question; ?></p>
     <?php endif; ?>
 
     <span class="phone-confirm-error"></span>
 
     <div class="phone-confirm__wrap">
         <input type="text"
-               name="<?= $field_name; ?>"
-               id="<?= $field_name; ?>"
+               name="<?php echo $field_name . '_'; ?>"
+               id="<?php echo $field_name . '_'; ?>"
                placeholder="****"
                class="contact-form__input">
         <span class="phone-confirm__btn check">Проверить</span>
         <span class="phone-confirm__btn send active">Получить код</span>
         <input type="hidden"
-               name="<?= $field_name; ?>"
-               id="<?= $field_name; ?>"
+               name="<?php echo $field_name; ?>"
+               id="<?php echo $field_name; ?>"
                value=""
             <?php if ($elmaName) : ?>
-                data-elma="<?= $elmaName; ?>"
+                data-elma="<?php echo $elmaName; ?>"
             <?php endif; ?>
             <?php if ($required) : ?>
                 data-req="true"
