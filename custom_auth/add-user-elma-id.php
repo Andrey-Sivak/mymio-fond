7774ef253ad57cb5b664e261501fdb90e18b8008
a.sivak <riziy_bes@mail.ru>
2022-04-20 17:34:31 +0200

    add api method for elma webHook

---
+++ custom_auth/add-user-elma-id.php
@@ -0,0 +1,45 @@
+<?php
+
+$user_email = $_POST['userEmail'];
+$elma_id = $_POST['elmaId'];
+
+$user_email = filter_var(trim($user_email), FILTER_SANITIZE_STRING);
+
+file_put_contents(__DIR__.'/data.log', "\n" . '-----------------------------------------------------------------' . "\n", FILE_APPEND); // LOG
+
+file_put_contents(__DIR__.'/data.log', date('d-m-Y H:i:s') . ' ' . $elma_id. ' ' .$user_email . "\n", FILE_APPEND); // LOG
+
+if (!$user_email || !$elma_id) {
+    exit();
+}
+
+file_put_contents(__DIR__.'/data.log', date('d-m-Y H:i:s') . ' ' . 'success get params' . "\n", FILE_APPEND); // LOG
+
+require_once('../Scripts/connect.php');
+
+$query = "SELECT * FROM `custom_users` WHERE `email` = '$user_email'";
+$result = $db->query($query);
+
+if (!$result->num_rows) {
+    $db->close();
+    echo false;
+    exit();
+}
+
+file_put_contents(__DIR__.'/data.log', date('d-m-Y H:i:s') . ' ' . 'success get user' . "\n", FILE_APPEND); // LOG
+
+$query = "UPDATE `custom_users` SET `elma_id` = '$elma_id' WHERE `email` = '$user_email'";
+$db->query($query);
+
+$db->close();
+
+file_put_contents(__DIR__.'/data.log', date('d-m-Y H:i:s') . ' ' . 'success update user elmaId' . "\n", FILE_APPEND); // LOG
+
+echo true;
+
+/*session_start();
+
+$_SESSION['user_name'] = $user_name;
+$_SESSION['user_email'] = $user_email;
+$_SESSION['user_pass'] = $db_user_pass;
+$_SESSION['elma_id'] = $elma_id;*/
