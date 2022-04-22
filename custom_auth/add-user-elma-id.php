1cf84e6794419e5f7edc8f4cf530484dc0422752
a.sivak <riziy_bes@mail.ru>
2022-04-22 22:17:02 +0200

    add name to api

--- custom_auth/add-user-elma-id.php
+++ custom_auth/add-user-elma-id.php
@@ -2,12 +2,13 @@
 
 $user_email = $_POST['userEmail'];
 $elma_id = $_POST['elmaId'];
+$user_name = $_POST['userName'];
 
 $user_email = filter_var(trim($user_email), FILTER_SANITIZE_STRING);
 
 file_put_contents(__DIR__.'/data.log', "\n" . '-----------------------------------------------------------------' . "\n", FILE_APPEND); // LOG
 
-file_put_contents(__DIR__.'/data.log', date('d-m-Y H:i:s') . ' ' . $elma_id. ' ' .$user_email . "\n", FILE_APPEND); // LOG
+file_put_contents(__DIR__.'/data.log', date('d-m-Y H:i:s') . ' ' . $elma_id . ' ' . $user_email . ' ' . $user_name . "\n", FILE_APPEND); // LOG
 
 if (!$user_email || !$elma_id) {
     exit();
@@ -28,7 +29,7 @@ if (!$result->num_rows) {
 
 file_put_contents(__DIR__.'/data.log', date('d-m-Y H:i:s') . ' ' . 'success get user' . "\n", FILE_APPEND); // LOG
 
-$query = "UPDATE `custom_users` SET `elma_id` = '$elma_id' WHERE `email` = '$user_email'";
+$query = "UPDATE `custom_users` SET `elma_id` = '$elma_id', `name` = '$user_name' WHERE `email` = '$user_email'";
 $db->query($query);
 
 $db->close();
