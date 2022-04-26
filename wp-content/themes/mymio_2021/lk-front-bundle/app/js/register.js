fa6c1f683056fefa0bcb97d58e4ef82716fca4c2
a.sivak <riziy_bes@mail.ru>
2022-04-27 01:16:41 +0200

    update register

--- wp-content/themes/mymio_2021/lk-front-bundle/app/js/register.js
+++ wp-content/themes/mymio_2021/lk-front-bundle/app/js/register.js
@@ -1,8 +1,14 @@
 'use strict';
 
 import {RegisterByApi} from "./registerByApi/RegisterByApi";
+import {RefusalByApi} from "./registerByApi/RefusalByApi";
 
 if (registerEmail) {
     const registerNewUser = new RegisterByApi(registerEmail);
+    registerNewUser.init();
+}
+
+if (wardRefusalEmail) {
+    const registerNewUser = new RefusalByApi(wardRefusalEmail);
     registerNewUser.init();
 }
