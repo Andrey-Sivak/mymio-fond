5e60006d7c869caf154f84837b109969974d9743
a.sivak <riziy_bes@mail.ru>
2022-04-27 01:14:54 +0200

    add year reset

--- api/usnet-medical-questionnaire.php
+++ api/usnet-medical-questionnaire.php
@@ -1,12 +1,14 @@
 <?php
 
 $user_email = $_POST['user_email'];
+$is_first_year = $_POST['is_first_year'];
 
 $user_email = filter_var(trim($user_email), FILTER_SANITIZE_STRING);
+$is_first_year = filter_var($is_first_year, FILTER_VALIDATE_BOOLEAN);
 
-file_put_contents(__DIR__ . '/unset-questionnaire.log', "\n" . '-----------------------------------------------------------------' . "\n", FILE_APPEND); // LOG
+file_put_contents(__DIR__ . '/unset-questionnaire.log', "\n" . '-----------------------------------------------------------------' . "\n\n", FILE_APPEND); // LOG
 
-file_put_contents(__DIR__ . '/unset-questionnaire.log', date('d-m-Y H:i:s') . ' ' . $user_email . "\n", FILE_APPEND); // LOG
+file_put_contents(__DIR__ . '/unset-questionnaire.log', date('d-m-Y H:i:s') . ' ' . $user_email . ' ' . $is_first_year . "\n", FILE_APPEND); // LOG
 
 if (!$user_email) {
     file_put_contents(__DIR__ . '/unset-questionnaire.log', date('d-m-Y H:i:s') . ' failed' . "\n", FILE_APPEND); // LOG
@@ -31,7 +33,12 @@ file_put_contents(__DIR__ . '/unset-questionnaire.log', date('d-m-Y H:i:s') . ' 
 
 $unsetted_locked = '0,';
 
-$query = "UPDATE `custom_users` SET `locked_tabs` = '$unsetted_locked' WHERE `email` = '$user_email'";
+if (isset($is_first_year)) {
+    $query = "UPDATE `custom_users` SET `locked_tabs` = '$unsetted_locked', `is_first_year` = '$is_first_year' WHERE `email` = '$user_email'";
+} else {
+    $query = "UPDATE `custom_users` SET `locked_tabs` = '$unsetted_locked' WHERE `email` = '$user_email'";
+}
+
 $db->query($query);
 
 $db->close();
