0262d721427e541a14a7de8d2ec0632ff5d990ce
a.sivak <riziy_bes@mail.ru>
2022-04-28 21:21:57 +0200

    add documents

--- wp-content/themes/mymio_2021/lk-front-bundle/app/js/classes/DocumentsClass.js
+++ wp-content/themes/mymio_2021/lk-front-bundle/app/js/classes/DocumentsClass.js
@@ -4,23 +4,57 @@ import {getUserDataFromElma} from "../api/elmaApi";
 
 export const DocumentsClass = function (parentBlock, elmaId) {
     const self = this;
+    this.documentsList = [];
+    this.container = $('.documents__wrap');
 
-    this.getDocs = () => {
+    this.getDocs = async () => {
         const url = `https://aeqlmvgvlxcee.elma365.ru/api/extensions/3d15932c-766e-4e91-b8ff-fed442649de2/script/ward/documents?id=${elmaId}`;
 
-        getUserDataFromElma(url, {
+        const result = await getUserDataFromElma(url, {
             method: 'GET',
-            mode: 'no-cors',
-        })
-            .then(res => res.json())
-            .then(data => console.log(data))
-            .catch(e => console.log(e));
+        });
+        const data = await result.json();
+
+        return await data.files;
     }
 
+    this.editNotice = () => {
+        const notice = $('.documents__notice');
 
+        if (notice.length) {
+            notice.html('Документы');
+        }
+    }
 
-    this.init = () => {
-        this.getDocs();
-        console.log(parentBlock);
+    this.createDocumentItem = (idx, documentTitle, documentUrl) => {
+        return $(`<div class="documents__item">
+                    <span class="documents__item_idx">${parseInt(idx) + 1}.</span>
+                    <p class="documents__item_title">${documentTitle}</p>
+                    <a href="${documentUrl}" class="documents__item_link">Скачать</a>
+                </div>`);
+    }
+
+    this.createDocumentsList = () => {
+        this.documentsList.forEach((d, i) => {
+            const elem = self.createDocumentItem(i, d.docname, d.url);
+            self.container.append(elem);
+        })
+    }
+
+    this.displayDocumentsList = () => {
+        if (!this.documentsList.length) {
+            return;
+        }
+
+        this.editNotice();
+        this.createDocumentsList();
+    }
+
+    this.init = async () => {
+        this.documentsList = await this.getDocs();
+
+        if (this.documentsList.length) {
+            this.displayDocumentsList();
+        }
     }
 }
