1b85596098ff20b0bf60112661e9803187ae3ef7
a.sivak <riziy_bes@mail.ru>
2022-04-29 15:54:19 +0200

    remove unnecessary files

--- wp-content/themes/mymio_2021/lk-front-bundle/app/js/helprers/questionaireProgress.js
+++
@@ -1,8 +0,0 @@
-export const questionaireProgress = () => {
-    const container = $('.lk-progress-inner');
-    const all = $('.lk-form[data-form="2"] .lk-form__tabs').children().length;
-    const locked = $('.lk-form__tab.locked').length;
-
-    const string = `${locked}/${all}`;
-    container.html(string);
-}
